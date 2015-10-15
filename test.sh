232c1f73631d27486da32e49f14b7c05572b0600
Adam Lerner <eletari@gmail.com>
2015-10-14 23:43:26 -0700

    Add generation of fake client_secret.json to test.sh.

--- test.sh
+++ test.sh
@@ -1,3 +1,15 @@
 npm install
 node --version
+echo '{
+    "web": {
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "client_id": "not_a_real_client_id",
+        "client_secret": "not_a_real_client_secret",
+        "redirect_uris": [
+            "http://localhost:9090/gmailRedirect"
+        ],
+        "token_uri": "https://accounts.google.com/o/oauth2/token"
+    }
+}' > client_secret.json
 npm test
