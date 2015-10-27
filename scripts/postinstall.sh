1e80d1a1d1738026dca68531ec91a88f385fdf0d
Eric Zeng <ericzeng2@gmail.com>
2015-10-26 17:06:24 -0700

    Make login page pretty
    - Add jquery
    - Login page style

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -10,3 +10,6 @@ set -x
 cp node_modules/bootstrap/dist/css/* src/web/css/3rdparty
 cp node_modules/bootstrap/dist/fonts/* src/web/fonts/3rdparty
 cp node_modules/bootstrap/dist/js/* src/web/js/3rdparty
+
+# jquery
+cp node_modules/jquery/dist/* src/web/js/3rdparty
