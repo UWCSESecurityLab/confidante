2e82f22c4587a2be403bb1f31fac803a8b15579a
Eric Zeng <ericzeng2@gmail.com>
2015-11-09 16:39:55 -0800

    Render autocomplete results by simply appending DOM elements

--- src/web/js/components/Autocomplete.react.js
+++ src/web/js/components/Autocomplete.react.js
@@ -13,15 +13,34 @@ var Autocomplete = React.createClass({
   updateKBTo: function(e) {
     this.setState({ kbto: e.target.value });
     keybaseAPI.autocomplete(this.state.kbto).then(function(body) {
-      this.setState({ results: body });
-      console.log(this.state.results);
+      this.setState({ results: JSON.parse(body) });
     }.bind(this));
   },
   render: function() {
-    return(
-      <div className="form-group">
-        <label htmlFor="kbto">Keybase ID of Recipient:</label>
-        <input type="text" value={this.state.kbto} name="kbto" onChange={this.updateKBTo} className="form-control"></input>
+    var renderedResults = [];
+    console.log('Current results: ');
+    console.log(this.state.results.completions);
+    if (this.state.results.completions) {
+      console.log('Yes we have completions, we should render them');
+      renderedResults = this.state.results.completions.map(function(result) {
+        return (
+          <li>
+            <p><strong>{result.components.full_name.val}</strong> {result.components.username.val}</p>
+          </li>
+        )
+      });
+    }
+    console.log(renderedResults);
+
+    return (
+      <div>
+        <div className="form-group">
+          <label htmlFor="kbto">Keybase ID of Recipient:</label>
+          <input type="text" value={this.state.kbto} name="kbto" onChange={this.updateKBTo} className="form-control"></input>
+        </div>
+        <ul>
+          {renderedResults}
+        </ul>
       </div>
     );
   }
