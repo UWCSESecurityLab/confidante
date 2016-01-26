e2c39f1e5727003ce0735a4ec46804c1d8ad3332
Eric Zeng <ericzeng@cs.washington.edu>
2016-01-25 17:16:28 -0800

    Fix logins by using URL escape characters in login_session query param
    The problem with logins was that getsalt.json returns a login_session,
    which is a base64 string. That string is sent back in a JSON body.
    login.json requires us to send that string back, however the base64
    encoding was not URL safe, which resulted in the bad session issue.
    
    Initially I tried using the URLSafeBase64 module to fix this, but it
    turns out that Keybase doesn't use the url safe base64 convention.
    I had to manually replace the non URL safe characters with the URL
    escape characters.
    
    The reason this probably regressed after migrating to XHR is because
    I think XHR doesn't automatically escape invalid URL characters for you,
    while the request module did.

--- src/web/js/keybaseAPI.js
+++ src/web/js/keybaseAPI.js
@@ -105,11 +105,20 @@ class KeybaseAPI {
       var hash = KeybaseAPI.computePasswordHash(this.passphrase, salt);
       var hmac_pwh = crypto.createHmac('SHA512', hash).update(login_session).digest('hex');
 
+      // We need to send the base64 encoded login session back to Keybase in the
+      // query string, so we need to make it URL safe. Annoyingly, Keybase
+      // doesn't use the conventional URL safe base64 encoding, instead we must
+      // replace the invalid characters with the URL escape characters.
+      let escapedLoginSession = saltDetails.login_session
+          .replace(/\+/g, '%2B')
+          .replace(/\//g, '%2F')
+          .replace(/\=/g, '%3D');
+
       xhr.post({
         url: this.serverBaseURI + '/login.json?' +
              'email_or_username=' + this.username + '&' +
              'hmac_pwh=' + hmac_pwh + '&' +
-             'login_session=' + saltDetails.login_session
+             'login_session=' + escapedLoginSession
       }, function (error, response, body) {
         if (error) {
           reject(body);
