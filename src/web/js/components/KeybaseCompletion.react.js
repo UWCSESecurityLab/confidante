b5856a41d050077afa3bdd47d67958ea472b5523
Eric Zeng <ericzeng@cs.washington.edu>
2016-02-05 16:30:02 -0800

    Make the Keybase cards look nice in autocomplete

--- src/web/js/components/KeybaseCompletion.react.js
+++ src/web/js/components/KeybaseCompletion.react.js
@@ -6,19 +6,19 @@ var KeybaseCompletion = React.createClass({
   render: function() {
     let user = this.props.user;
     return (
-      <div className="keybase-card row" onClick={this.props.onClick}>
+      <div className="keybase-card" onClick={this.props.onClick}>
         <div className="pic-section">
           <img className="profile-pic" src={ user.picture }></img>
         </div>
         <div className="text-section">
-          <div className="info-line">
+          <div className="name-line">
             <h4 className="line-item">{ user.full_name ? user.full_name : user.username}</h4>
             <a className="btn btn-warning keybase-btn" role="button" target="_blank"
                href={ 'https://keybase.io/' + user.username }>
-              View profile on Keybase
+              Keybase Profile
             </a>
           </div>
-          <div className="info-line">
+          <div className="accounts-line">
             { user.twitter ?
               <span className="line-item">
                 <img src="twitter.png" height="24px"></img>
