2843b3dca336d718d6e5b3510afe8791dbc3feb1
Eric Zeng <ericzeng@cs.washington.edu>
2016-03-14 16:18:50 -0700

    Babelify the KeybaseAPI bundle so that it can be loaded in Safari
    KeybaseAPI needs babelify because it wasn't working with arrow functions
    in Safari.

--- scripts/watchify.sh
+++ scripts/watchify.sh
@@ -3,4 +3,4 @@ mkdir -p gen
 watchify -t babelify src/web/js/inbox.js -o gen/inbox-bundle.js --insert-globals --debug -v & \
 watchify -t babelify src/web/js/invite.js -o gen/invite-bundle.js --insert-globals --debug -v & \
 watchify -t babelify src/web/js/signup.js -o gen/signup-bundle.js --insert-globals --debug -v & \
-watchify src/web/js/keybaseAPI.js -o gen/keybaseAPI-bundle.js -s KeybaseAPI --insert-globals --debug -v
+watchify -t babelify src/web/js/keybaseAPI.js -o gen/keybaseAPI-bundle.js -s KeybaseAPI --insert-globals --debug -v
