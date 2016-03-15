d9a0396baeeba432f796bea2a50a6871cfc46516
Eric Zeng <ericzeng@cs.washington.edu>
2016-03-15 16:36:48 -0700

    Fix styling and interactivity for dropdowns

--- src/web/js/components/ContactCompletion.react.js
+++ src/web/js/components/ContactCompletion.react.js
@@ -4,8 +4,20 @@ var React = require('react');
 
 var ContactCompletion = React.createClass({
   render: function() {
+    let contact = this.props.data;
+
+    let className = 'completion';
+    if (this.props.isSelected) {
+      className += ' completion-selected';
+    }
+
     return (
-      <div>{this.props.data.name} {this.props.data.value}</div>
+      <li key={contact.email + contact.name}
+          className={className}>
+        { contact.name.length != 0 ?
+          <span id="name">{ contact.name } - </span> : null }
+          <span id="email">{ contact.email }</span>
+      </li>
     );
   }
 });
--- src/web/js/components/KeybaseCard.react.js
+++ src/web/js/components/KeybaseCard.react.js
@@ -5,8 +5,14 @@ var React = require('react');
 var KeybaseCompletion = React.createClass({
   render: function() {
     let user = this.props.data;
+
+    let className = 'keybase-card';
+    if (this.props.isSelected) {
+      className += ' keybase-card-selected';
+    }
+
     return (
-      <div className="keybase-card" onClick={this.props.onClick}>
+      <div className={className} onClick={this.props.onClick}>
         <div className="pic-section">
           <img className="profile-pic" src={ user.picture }></img>
         </div>
