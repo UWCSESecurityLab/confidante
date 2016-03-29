f407a74e518f827b22d135c030cba19d3004432a
Eric Zeng <ericzeng@cs.washington.edu>
2016-03-29 14:33:51 -0700

    Add better docs for InboxActions

--- src/web/js/actions/InboxActions.js
+++ src/web/js/actions/InboxActions.js
@@ -3,43 +3,71 @@
 var InboxDispatcher = require('../dispatcher/InboxDispatcher');
 
 module.exports = {
-  // ComposeStore actions
+  /**
+   * Sets the 'To:' field in ComposeArea to the specified value.
+   * @param message.message The JSON Gmail message containing metadata info
+   * @param message.replyAll A boolean, true if message is a reply all
+   */
   setInReplyTo: function(message) {
     InboxDispatcher.dispatch({
       type: 'SET_IN_REPLY_TO',
       message: message
     });
   },
+
+  /**
+   * Toggles the ComposeArea between invites and regular emails.
+   * @param message True if should show invite dialogue, false otherwise
+   */
   setInvite: function(message) {
     InboxDispatcher.dispatch({
       type: 'SET_INVITE',
       message: message
     });
   },
+
+  /**
+   * Reset the entire ComposeStore.
+   */
   resetComposeFields: function() {
     InboxDispatcher.dispatch({ type: 'RESET_FIELDS' });
   },
 
-  // MessageStore actions
+  /**
+   * Mark a thread as read.
+   * @param threadId The id of the Gmail thread.
+   */
   markAsRead: function(threadId) {
     InboxDispatcher.dispatch({
       type: 'MARK_AS_READ',
       message: threadId
     });
   },
+
+  /**
+   * Fetch new emails from Gmail.
+   */
   refresh: function() {
     InboxDispatcher.dispatch({
       type: 'REFRESH'
     });
   },
 
-  // AutocompleteStore actions
+  /**
+   * Search for Google contacts matching the query.
+   * @param query A string containing the search query.
+   */
   getContacts: function(query) {
     InboxDispatcher.dispatch({
       type: 'GET_CONTACTS',
       query: query
     });
   },
+
+  /**
+   * Search for Keybase users matching the query.
+   * @param query A string containing the serach query.
+   */
   getKeybase: function(query) {
     InboxDispatcher.dispatch({
       type: 'GET_KEYBASE',
