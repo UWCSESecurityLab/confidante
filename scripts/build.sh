8bea38ee213a315951a0e05bcd1167eaf959b7ad
Eric Zeng <ericzeng@cs.washington.edu>
2016-06-22 14:00:47 -0700

    Update Browserify and Babelify
    Need to use plugins for es6/react with Babelify now

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p gen
 
-browserify -t babelify src/web/js/login.js -o gen/login-bundle.js --insert-globals --debug
-browserify -t babelify src/web/js/inbox.js -o gen/inbox-bundle.js --insert-globals --debug
-browserify -t babelify src/web/js/invite.js -o gen/invite-bundle.js --insert-globals --debug
-browserify -t babelify src/web/js/signup.js -o gen/signup-bundle.js --insert-globals --debug
+browserify -t [ babelify --presets [ es2015 react ] ] src/web/js/login.js -o gen/login-bundle.js --insert-globals --debug
+browserify -t [ babelify --presets [ es2015 react ] ] src/web/js/inbox.js -o gen/inbox-bundle.js --insert-globals --debug
+browserify -t [ babelify --presets [ es2015 react ] ] src/web/js/invite.js -o gen/invite-bundle.js --insert-globals --debug
+browserify -t [ babelify --presets [ es2015 react ] ] src/web/js/signup.js -o gen/signup-bundle.js --insert-globals --debug
--- scripts/watchify.sh
+++ scripts/watchify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p gen
-watchify -t babelify src/web/js/inbox.js -o gen/inbox-bundle.js --insert-globals --debug -v & \
-watchify -t babelify src/web/js/invite.js -o gen/invite-bundle.js --insert-globals --debug -v & \
-watchify -t babelify src/web/js/signup.js -o gen/signup-bundle.js --insert-globals --debug -v & \
-watchify -t babelify src/web/js/login.js -o gen/login-bundle.js --insert-globals --debug -v
+watchify -t [ babelify --presets [ es2015 react ] ] src/web/js/inbox.js -o gen/inbox-bundle.js --insert-globals --debug -v & \
+watchify -t [ babelify --presets [ es2015 react ] ] src/web/js/invite.js -o gen/invite-bundle.js --insert-globals --debug -v & \
+watchify -t [ babelify --presets [ es2015 react ] ] src/web/js/signup.js -o gen/signup-bundle.js --insert-globals --debug -v & \
+watchify -t [ babelify --presets [ es2015 react ] ] src/web/js/login.js -o gen/login-bundle.js --insert-globals --debug -v
