e6bf1dc35315de68d6ac8b8b8ebcfee62c053fda
Eric Zeng <ericzeng@cs.washington.edu>
2017-03-24 15:19:11 -0700

    Change filenames of Electron bundles (since they aren't mutually compatible)

--- scripts/build-electron.sh
+++ scripts/build-electron.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 mkdir -p gen
 
-browserify -t [ babelify --presets [ es2015 react ] ] src/web/js/login.js -o gen/login-bundle.js --debug --ignore-missing --no-builtins --no-commondir --insert-global-vars=\"global\" --no-browser-field
-browserify -t [ babelify --presets [ es2015 react ] ] src/web/js/inbox.js -o gen/inbox-bundle.js --debug --ignore-missing --no-builtins --no-commondir --insert-global-vars=\"global\" --no-browser-field
-browserify -t [ babelify --presets [ es2015 react ] ] src/web/js/invite.js -o gen/invite-bundle.js --debug --ignore-missing --no-builtins --no-commondir --insert-global-vars=\"global\" --no-browser-field
-browserify -t [ babelify --presets [ es2015 react ] ] src/web/js/signup.js -o gen/signup-bundle.js --debug --ignore-missing --no-builtins --no-commondir --insert-global-vars=\"global\" --no-browser-field
-browserify -t [ babelify --presets [ es2015 react ] ] src/web/js/handleOAuth.js -o gen/handleOAuth-bundle.js --debug --ignore-missing --no-builtins --no-commondir --insert-global-vars=\"global\" --no-browser-field
+browserify -t [ babelify --presets [ es2015 react ] ] src/web/js/login.js -o gen/login-electron.js --debug --ignore-missing --no-builtins --no-commondir --insert-global-vars=\"global\" --no-browser-field
+browserify -t [ babelify --presets [ es2015 react ] ] src/web/js/inbox.js -o gen/inbox-electron.js --debug --ignore-missing --no-builtins --no-commondir --insert-global-vars=\"global\" --no-browser-field
+browserify -t [ babelify --presets [ es2015 react ] ] src/web/js/signup.js -o gen/signup-electron.js --debug --ignore-missing --no-builtins --no-commondir --insert-global-vars=\"global\" --no-browser-field
