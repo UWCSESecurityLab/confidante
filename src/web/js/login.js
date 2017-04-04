881c3a2815bf6f288e0c8e8ba88e65f7dce55ec4
Adam Lerner <eletari@gmail.com>
2017-04-04 16:40:38 -0700

    login.js redirects to Google instead of directly to /mail.
    Avoids a redirect loop if you go /login > /mail > no Gmail creds
    > back to login.

--- src/web/js/login.js
+++ src/web/js/login.js
@@ -3,11 +3,20 @@ const Login = require('./components/Login.react');
 const React = require('react');
 const ReactDOM = require('react-dom');
 const KeybaseAPI = require('./keybaseAPI');
+const GoogleOAuth = require('../../googleOAuth');
 const flags = require('../../flags');
+
+function redirectToGoogle() {
+  if (flags.ELECTRON) {
+    ipcRenderer.send('google-redirect');
+  } else {
+    window.location.href = GoogleOAuth.getAuthUrl();
+  }
+}
 
 if (!flags.ELECTRON) {
   if (KeybaseAPI.keybaseLoggedIn()) {
-    window.location.href = '/mail';
+    redirectToGoogle();
   }
 }
 
