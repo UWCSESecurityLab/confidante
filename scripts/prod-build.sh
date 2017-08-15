80523740cfb67d5fbc198442875fdb72747804bf
Eric Zeng <ericzeng@cs.washington.edu>
2017-08-15 09:23:28 -0700

    Revert dependency updates for transpiler toolchain
    Trying to fix web version being non-responsive

--- scripts/prod-build.sh
+++ scripts/prod-build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p gen
 
-browserify -t [ envify --NODE_ENV production ] -t [ babelify --presets [ es2015 react ] ] src/web/js/login.js -o gen/login-bundle.js
-browserify -t [ envify --NODE_ENV production ] -t [ babelify --presets [ es2015 react ] ] src/web/js/inbox.js -o gen/inbox-bundle.js
-browserify -t [ envify --NODE_ENV production ] -t [ babelify --presets [ es2015 react ] ] src/web/js/invite.js -o gen/invite-bundle.js
-browserify -t [ envify --NODE_ENV production ] -t [ babelify --presets [ es2015 react ] ] src/web/js/signup.js -o gen/signup-bundle.js
-browserify -t [ envify --NODE_ENV production ] -t [ babelify --presets [ es2015 react ] ] src/web/js/handleOAuth.js -o gen/handleOAuth-bundle.js
+browserify -t [ envify --NODE_ENV production ] -t [ babelify --presets [ es2015 react ] ] src/web/js/login.js | uglifyjs --compress -o gen/login-bundle.js
+browserify -t [ envify --NODE_ENV production ] -t [ babelify --presets [ es2015 react ] ] src/web/js/inbox.js | uglifyjs --compress -o gen/inbox-bundle.js
+browserify -t [ envify --NODE_ENV production ] -t [ babelify --presets [ es2015 react ] ] src/web/js/invite.js | uglifyjs --compress -o gen/invite-bundle.js
+browserify -t [ envify --NODE_ENV production ] -t [ babelify --presets [ es2015 react ] ] src/web/js/signup.js | uglifyjs --compress -o gen/signup-bundle.js
+browserify -t [ envify --NODE_ENV production ] -t [ babelify --presets [ es2015 react ] ] src/web/js/handleOAuth.js | uglifyjs --compress -o gen/handleOAuth-bundle.js
